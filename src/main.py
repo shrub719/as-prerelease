695e8ad32ffef7253bc880e31cde9a761a3ed0c1
shrub719 <shrub719@gmail.com>
2025-03-18 14:42:27 +0000

    code

--- src/main.py
+++ src/main.py
@@ -256,6 +256,7 @@ def DisplayFind(Map, Pirate, ItemFound):
 def PirateDigs(Map, HiddenMap, Pirate):
     if HiddenMap[Pirate.Row][Pirate.Column] != SAND:
         DisplayFind(Map, Pirate, HiddenMap[Pirate.Row][Pirate.Column])
+        HiddenMap[Pirate.Row][Pirate.Column] = SAND
     else:
         print("Nothing found")
     Pirate.Score -= 10
