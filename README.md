162d8a3ec0c32eb7572b8c5e41839b8df67f0478
shrub719 <shrub719@gmail.com>
2025-03-20 09:38:00 +0000

    edited root files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Don't Dig Twice
 
 > Currently, you are able to dig the same location (regardless of what is at the location) an infinite amount of times. Amend the code so that each location can only be dug once. You will have to include functionality around editing the hidden map in your solution.
+> 
+> ADDENDUM 1: The user should see the locations they have dug already. If they have dug an item, the user should simply see that that location has been dug.
 
 Program amended to completely prevent the pirate from digging
 into the same spot they have dug before.
--- dont-dig-twice-2.py
+++ dont-dig-twice-2.py
@@ -1,12 +1,31 @@
 HOLE = 'O'  # new constant for special spaces in HiddenMap
             # that have already been dug
 
+def DisplayFind(Map, Pirate, ItemFound):
+    # removed the already displayed coin/coconut icons when dug
+    if ItemFound == COCONUT:
+        Item = "Coconut"
+        Pirate.Score += 10
+    elif ItemFound == TREASURE:
+        Item = "Treasure chest"
+        Pirate.TreasureFound = True
+        Pirate.Score += 200
+    elif ItemFound == GOLD_COIN:
+        Item = "Gold coin"
+        Pirate.NumberOfCoinsFound += 1
+        print("The treasure must be nearby")
+    else:
+        Item = "Unidentified item"
+    print(f"Found {Item}")
+
 def PirateDigs(Map, HiddenMap, Pirate):
     HiddenDug = HiddenMap[Pirate.Row][Pirate.Column]
     if HiddenDug not in [SAND, HOLE]:
         DisplayFind(Map, Pirate, HiddenDug)
         # set the dug position to a special "already-dug" character
         HiddenMap[Pirate.Row][Pirate.Column] = HOLE
+        # also set it in the actual map
+        Map[Pirate.Row][Pirate.Column] = HOLE
     elif HiddenDug == HOLE:
         # triggered when digging again in a dug spot
         print("Cannot dig in same spot twice")
@@ -14,6 +33,7 @@ def PirateDigs(Map, HiddenMap, Pirate):
         print("Nothing found")
         # create hole even if digging sand
         HiddenMap[Pirate.Row][Pirate.Column] = HOLE
+        Map[Pirate.Row][Pirate.Column] = HOLE
     if HiddenDug != HOLE:
         # only take away score and time if actually dug
         Pirate.Score -= 10
