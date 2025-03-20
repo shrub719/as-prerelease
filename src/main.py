0ec5ecc2dda506aa19036e5a74e6cb4f15adbf57
shrub719 <shrub719@gmail.com>
2025-03-20 14:10:30 +0000

    completed

--- src/main.py
+++ src/main.py
@@ -26,12 +26,14 @@ class MapSizeRecord:
 
 class PirateRecord:
     def __init__(self):
+        # added property
         self.Row = 0
         self.Column = 0
         self.Score = 100
         self.DigTime = 0.0
         self.TreasureFound = False
         self.NumberOfCoinsFound = 0
+        self.UsedDynamite = False
 
 def ResetMapSize(MapSize):
     MapSize.Rows = MAX_ROWS
@@ -45,12 +47,14 @@ def ResetMaps(Map, HiddenMap):
             HiddenMap[Row][Column] = SAND
 
 def ResetPirateRecord(Pirate):
+    # added property
     Pirate.Row = 0
     Pirate.Column = 0
     Pirate.Score = 100
     Pirate.DigTime = 0.0
     Pirate.TreasureFound = False
     Pirate.NumberOfCoinsFound = 0
+    Pirate.UsedDynamite = False
 
 def GenerateMap(Map, MapSize):
     FileIn = open("MapData.txt", 'r')
@@ -261,14 +265,48 @@ def PirateDigs(Map, HiddenMap, Pirate):
     Pirate.Score -= 10
     Pirate.DigTime += 1.75
 
+def GetSurroundingSquares(Map, HiddenMap, Pirate):
+    # get indexes of surrounding squares
+    Col = Pirate.Column
+    Row = Pirate.Row
+    Indexes = [[
+        (Row+i, Col-1),
+        (Row+i, Col),
+        (Row+i, Col+1)
+    ] for i in range(-1,2)]
+    Indexes[1].pop(1)  # removes location of pirate
+
+    print("BOOM!")
+    for Row in Indexes:
+        for Square in Row:
+            FoundItem = HiddenMap[Square[0]][Square[1]]
+            if FoundItem == TREASURE:
+                # delete treasure
+                HiddenMap[Square[0]][Square[1]] = SAND
+                print("oops...")
+            elif FoundItem != SAND:
+                # claims each item in the surrounding squares
+                DisplayFind(Map, Pirate, FoundItem)
+                Map[Pirate.Row][Pirate.Column] = SAND  # replace "discovered" items on pirate square
+    Pirate.UsedDynamite = True
+
+def PirateUsesDynamite(Map, HiddenMap, Pirate):
+    if Pirate.UsedDynamite:
+        print("You have already used your dynamite.")
+        return
+    GetSurroundingSquares(Map, HiddenMap, Pirate)
+
 def GetPirateAction(Map, MapSize, HiddenMap, Pirate, Answer):
-    Answer = input("Pirate to walk (W) or dig (D), to finish game press Enter: ")
-    while not (Answer == "W" or Answer == "D" or Answer == PRESSED_ENTER):
-        Answer = input("Pirate to walk (W) or dig (D), to finish game press Enter: ")
+    # added new dynamite (X) option
+    Answer = input("Pirate to walk (W) or dig (D), or use dynamite (X), to finish game press Enter: ")
+    while not (Answer == "W" or Answer == "D" or Answer == PRESSED_ENTER or Answer == "X"):
+        Answer = input("Pirate to walk (W) or dig (D), or use dynamite (X), to finish game press Enter: ")
     if Answer == "W":
         PirateWalks(Map, MapSize, HiddenMap, Pirate)
     elif Answer == "D":
         PirateDigs(Map, HiddenMap, Pirate)
+    elif Answer == "X":
+        PirateUsesDynamite(Map, HiddenMap, Pirate)
     return Answer
 
 def DisplayResults(Pirate):
