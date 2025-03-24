b3a29ba5a4486894ec51e6b854ac611a8de65f5e
shrub719 <shrub719@gmail.com>
2025-03-24 09:56:39 +0000

    fix editing list while iterating over it

--- src/main.py
+++ src/main.py
@@ -264,30 +264,30 @@ def PirateDigs(Map, HiddenMap, Pirate):
 
 def Flood(Map, MapSize):
     # FIX
-    for r, Row in enumerate(Map):
-        for c, Square in enumerate(Row):
-            if Square == WATER:
+    for r in range(len(Map)):
+        for c in range(len(Map[0])):
+            if Map[r][c] == WATER:
                 if r != 0: Map[r-1][c] = FLOOD  # flood above
                 if r >= MapSize.Rows: Map[r+1][c] = FLOOD  # flood below
                 if c != 0: Map[r][c-1] = FLOOD  # flood left
                 if c <= MapSize.Columns: Map[r][c+1] = FLOOD  # flood right
 
-    for r, Row in enumerate(Map):
-        for c, Square in enumerate(Row):
-            if Square == FLOOD:
+    for r in range(len(Map)):
+        for c in range(len(Map[0])):
+            if Map[r][c] == FLOOD:
                 Map[r][c] = WATER
 
 
 def GetPirateAction(Map, MapSize, HiddenMap, Pirate, Answer):
     Answer = input("Pirate to walk (W) or dig (D), to finish game press Enter: ")
+    # add flooding function
+    Flood(Map, MapSize)
     while not (Answer == "W" or Answer == "D" or Answer == PRESSED_ENTER):
         Answer = input("Pirate to walk (W) or dig (D), to finish game press Enter: ")
     if Answer == "W":
         PirateWalks(Map, MapSize, HiddenMap, Pirate)
     elif Answer == "D":
         PirateDigs(Map, HiddenMap, Pirate)
-    # add flooding function
-    Flood(Map, MapSize)
     return Answer
 
 def DisplayResults(Pirate):
